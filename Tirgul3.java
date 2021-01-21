a39d48b54f61908a1fc0d24ede49733afb1d47e1
soozijan <75090137+soozijan@users.noreply.github.com>
2021-01-21 15:44:28 +0200

    Create Tirgul3.java

---
+++ Tirgul3.java
@@ -0,0 +1,99 @@
+public class Main {
+    static Scanner scanner = new Scanner (System.in);
+
+    public static <k> void main(String[] args) {
+       // greet ();
+       // int number = 5;
+       // printNum (number);
+      //  for (int index = 0; index <= 10; index++) {
+       //     System.out.println (index);
+       // }
+
+     //  int k = 5;
+      //  changeNumber (k);
+      //  System.out.println (k);
+
+       //Targil 7:
+      //  int x = 3;
+      //  int y = 7;
+      //  moltiply (3,7);
+      //  moltiply (5,4);
+       // moltiply (3,6);
+       // moltiply (7,5);
+
+
+        //Targil 8:
+      //  int a = 3;
+      //  int b = 2;
+      //  calc(3,2, '+');
+      //  calc (3,2,'-');
+
+        //Targil 9:
+      //  int number_1 = 6;
+      //  int number_2 = 4;
+     //   int number_3 = 2;
+      //  System.out.println (threeNumbers(6,4,2));
+     //   threeNumbers(6,4,2);
+
+
+        //Targil 10:
+        int[]numbers = {12, 18, 20,25,10};
+        int x1 = 9;
+        int counter = 0;
+        arryNum (numbers, x1);
+
+}
+    //Targil 10:
+    private static boolean arryNum(int[] numbers, int x1) {
+        for (int index = 0; index < numbers.length; index++) {
+            int counter = 0;
+            if (counter == index) {
+                return (true);
+            } else {
+                return  (false);
+            }
+
+        }
+
+
+           }
+
+
+
+      //Targil 9:
+   //private static float threeNumbers(int number_1, int number_2, int number_3) {
+       // return (number_1+ number_2 +number_3 ) / 3;
+    //}
+
+
+    //Targil 4,5:
+        // private static void printNum(int number)  {
+         //   System.out.println (number);
+        //  }
+
+
+        //Targil 3:
+        // private static void greet() {
+         //   System.out.println ("good Morning");
+        // }
+
+         //Targil 6:
+   // private static void changeNumber(int k) {
+      //  k++;
+     //   System.out.println (k);
+    //}
+
+      //Targil 7:
+   // private static void moltiply(int x, int y)   {
+       // System.out.println (x*y);
+    //}
+
+    //Targil 8:
+    //private static void calc(int a, int b,char c)   {
+        //   if(c == '+')   {
+            //    System.out.println ( a +  b);
+          //  }
+          //  if (c == '-')   {
+          //      System.out.println (a - b);
+         //   }
+
